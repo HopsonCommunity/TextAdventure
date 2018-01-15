72660ea69b5760e563fee2894716e4b26bdcdf7a
Unknown <ricoujules69@gmail.com>
2018-01-15 16:27:20 +0100

    Import all class into the main file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 """ Main file - mainly for debugging and testing """
 
+from player import Player
+from story import Story
+from state import State  
+
 if __name__ == '__main__':
-    print "Hello World !"
+    print("Hello World !")
