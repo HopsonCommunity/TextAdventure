5a0fdcd44d1489757bd6d7b7c5f524aeb72e0089
Jakub Jendryka <jendryka.j@gmail.com>
2018-01-19 13:05:16 +0100

    Changed player history methods to work with dictionaries

--- src/player.py
+++ src/player.py
@@ -39,13 +39,17 @@ class Player(object):
         """Remove all items of 'items' (dictionary name to value)"""
         pass
 
-    def set_history(self, name, value):
+    def set_history(self, history: dict):
         """Set the history 'name' to 'value', it must be a boolean"""
-        self.history[name] = value
+        for story, value in history.items():
+            self.history[story] = value
 
-    def has_history(self, name):
-        """Return true if the history 'name' has been previously set"""
-        return self.history.get(name, False)
+    def has_history(self, history: dict):
+        """Return true if all values in player history and in parameter are equal"""
+        for story, value in history.items():
+            if self.history.get(story, 0) != value:
+                return False
+            return True
 
 def load_player_from(content):
     """Return a Player object load from the save file"""
