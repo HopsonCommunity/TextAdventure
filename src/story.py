79a847d10b8725a161f10ce13e19432c92cc55f3
Vlad Ionescu <ionescu.vlad.2010@gmail.com>
2018-02-10 04:18:23 +0200

    Add type hints for the Story class
    Signed-off-by: Vlad Ionescu <ionescu.vlad.2010@gmail.com>

--- src/story.py
+++ src/story.py
@@ -1,5 +1,8 @@
 """ Story class file """
 
+import json
+from state import State
+
 class Story(object):
     """ Describe a story.
     States, items, traits, class
@@ -14,26 +17,26 @@ class Story(object):
         pass
 
 
-    def add_item(self, name, in_game_name, description):
+    def add_item(self, name: str, in_game_name: str, description: str):
         """Add an item to the story"""
         pass
 
 
-    def add_trait(self, name, in_game_name, decription, antonym=None):
+    def add_trait(self, name: str, in_game_name: str, decription: str, antonym=None):
         """Add a trait, aswell as the antonym if it's not None"""
         pass
 
 
-    def add_class(self, class_name, trait_value_dict):
+    def add_class(self, class_name: str, trait_value_dict: dict):
         """Add a class, trait_value_dict is a dictionary 'trait' to 'value'"""
         pass
 
 
-    def add_state(self, name, state):
+    def add_state(self, name: str, state: State):
         """Add the state to the story, 'state' must be a State object"""
         pass
 
 
-def load_story_from_json(json_content):
+def load_story_from_json(json_content: json):
     """Return a Story object load from the json"""
     pass
